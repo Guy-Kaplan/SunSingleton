64635981efa07c7a7cc5a6cc845be8f01a7886a4
Guy-Kaplan <39667435+Guy-Kaplan@users.noreply.github.com>
2019-12-11 23:44:14 +0200

    Adding main program

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+namespace SunSingleton
+{
+    public class Program
+    {
+        // Made by Guy Kaplan
+        // Sun Singleton DP (Design Pattern) in C#
+        static void Main(string[] args)
+        {
+            var msg = new StringBuilder();
+            msg.AppendLine("---Made by Guy Kaplan---");
+            msg.Append("This program is an example of the Singleton DP ");
+            msg.AppendLine("(Design Pattern)");
+            msg.AppendLine("(There is ONLY ONE Sun object in this program)");
+            Console.WriteLine(msg.ToString());
+
+            PrintCodeAndResult("var sun = new Sun();", false);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("--Compilation error-- (Sun is a Singleton...)");
+            Console.ResetColor();
+            Console.WriteLine();
+
+            PrintCodeAndResult("Sun.sun.Shine();");
+            Sun.sun.Shine(); // The Singleton ONLY instance
+
+            PrintCodeAndResult("Sun.sun.PrintDetails();");
+            Sun.sun.PrintDetails(); // The Singleton ONLY instance
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }//end of main
+
+        /// <summary>
+        /// Prints the code tried and its result
+        /// </summary>
+        /// <param name="code">The code that has been executed</param>
+        public static void PrintCodeAndResult(string code, bool addComment = true)
+        {
+            Console.Write($"Code: ");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write($"{code}");
+            if (addComment)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(" // The Singleton ONLY instance");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+            Console.WriteLine("Result:");
+        }
+    }
+}
---
+++ Sun.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace SunSingleton
+{
+    // Singleton
+    public class Sun
+    {
+        public static readonly Sun sun = new Sun(); // The ONLY Sun instance
+        public readonly string Color;
+        public readonly int RadiusInKm;
+        public readonly double AgeInBillionYears;
+
+        // private makes sure that a Sun object will NOT be created outside this class
+        // Namely, there is ONLY ONE Sun object = Singleton
+        private Sun()
+        {
+            Color = "Yellow";
+            RadiusInKm = 696342;
+            AgeInBillionYears = 4.6;
+        }
+
+        /// <summary>
+        /// Prints a yellow sunshine message
+        /// </summary>
+        public void Shine()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"The Sun is shining! :){Environment.NewLine}");
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Prints the Sun ONLY instance's details
+        /// </summary>
+        public void PrintDetails()
+        {
+            var details = new StringBuilder();
+            details.AppendLine($"Color: {Color}");
+            details.AppendLine($"Radius: {RadiusInKm} km");
+            details.AppendLine($"Age: {AgeInBillionYears} billion years");
+            Console.WriteLine(details.ToString());
+        }
+    }
+}
